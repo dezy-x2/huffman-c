aeb0758292032e9d6f49434487614b272bc99c6a
Daniel Desmond <df.dezmond9@gmail.com>
2022-03-21 16:18:20 -0400

    added helpers to initialize nodes

--- huffman.c
+++ huffman.c
@@ -7,11 +7,32 @@ typedef struct letterNode {
 } letterNode;
 
 typedef struct treeNode {
-    letterNode* c;
+    letterNode* ln;
     struct treeNode* left;
     struct treeNode* right;
 } treeNode;
 
+void initializeLetterNode(letterNode* ln, int f, char c);
+void initializeTreeNode(treeNode* tn, letterNode* ln, treeNode* leftTn, treeNode* rightTn);
+
 int main() {
     printf("Hello World!\n");
+    letterNode aNode;
+    treeNode aTree;
+    
+    initializeLetterNode(&aNode, 8, 'a');
+    initializeTreeNode(&aTree, &aNode, NULL, NULL);
+}
+
+void initializeLetterNode(letterNode* ln, int f, char c) {
+    printf("Initializing letter node with %d freq and '%c' char\n", f,c);
+    ln->freq = f;
+    ln->c=c;
+}
+
+void initializeTreeNode(treeNode* tn, letterNode* ln, treeNode* leftTn, treeNode* rightTn) {
+    printf("Initializing tree node for letter '%c'\n",ln->c);
+    tn->ln = ln;
+    tn->left = leftTn;
+    tn->right = rightTn;
 }
