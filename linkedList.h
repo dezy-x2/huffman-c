fd5e67841d6f12f337744dfe0d1a0a784478001b
Daniel Desmond <df.dezmond9@gmail.com>
2022-03-28 13:27:32 -0400

    COMMENTS COMMENTS COMMENTS COMMENTS COMMENTS COMM-

--- linkedList.h
+++ linkedList.h
@@ -1,10 +1,20 @@
 #include "huffman.h"
 
+/**
+ * @brief the basic node that holds its next node and one letterNode
+ * @param pNext - (Node*) the next node in the linkedlist
+ * @param pData - (LetterNode*) the letter node that this node stores
+ */
 typedef struct Node {
     struct Node* pNext;
     LetterNode* pData;
 } Node;
 
+/**
+ * @brief parents struct that holds the first node and a bit of data about the whole thing
+ * @param pHeadNode - (Node*) the first node in the list
+ * @param numNodes - (int) how many nodes are in the list
+ */
 typedef struct LinkedList {
     Node* pHeadNode;
     int numNodes;
