1ffce33f8bc6f798c9ede8c1b25b410a40cfa361
Daniel Desmond <df.dezmond9@gmail.com>
2022-03-29 14:10:05 -0400

    added initializers to main linked list file

--- linkedList.c
+++ linkedList.c
@@ -6,3 +6,21 @@
 int main() {
     printf("Hello World\n");
 }
+
+Node* initializeNode(Node* nextNode, LetterNode* data) {
+    Node* newNode = (Node*) calloc(1, sizeof(Node));
+    newNode -> pNext = nextNode;
+    newNode -> pData = data;
+    return newNode;
+}
+
+LinkedList* initializeLinkedList(Node* headNode) {
+    LinkedList* newList = (LinkedList*) calloc(1, sizeof(LinkedList));
+    newList -> pHeadNode = headNode;
+    if (headNode != NULL) {
+        newList->numNodes = 1;
+    } else {
+        newList->numNodes = 0;
+    }
+    return newList;
+}
