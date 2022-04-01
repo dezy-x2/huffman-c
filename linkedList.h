36de9de8e7654214a9124c8c3a162e24cfc5b23d
Daniel Desmond <df.dezmond9@gmail.com>
2022-04-01 10:18:37 -0400

    added function declaration for swap()

--- linkedList.h
+++ linkedList.h
@@ -30,5 +30,5 @@ LinkedList* initializeLinkedList();
 void add(LinkedList* list, Node* newNode);
 void addOrIncrement(LinkedList* list, char c);
 void printList(LinkedList* list);
-// need to add a swap function
 Node* get(LinkedList* list, int pos);
+void swap(LinkedList* list, Node* node1, Node* node2);
