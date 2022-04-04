8cbc3ac27209251f5944602ae55139bac5674502
Daniel Desmond <df.dezmond9@gmail.com>
2022-04-04 18:12:28 -0400

    swap and bubble sort now work!! this is huge :)

--- linkedList.c
+++ linkedList.c
@@ -50,6 +50,7 @@ void printList(LinkedList* list) {
 }
 
 Node* get(LinkedList* list, int pos) {
+    if (pos < 0) return NULL;
     Node* curr = list->pHeadNode;
     int count = 0;
     printf("\nRUNNING GET\n");
@@ -62,28 +63,10 @@ Node* get(LinkedList* list, int pos) {
 }
 
 void swap(LinkedList* list, Node* node1, Node* node2) {
-    Node* node1Prev = get(list, idxOf(list, node1)-1);
-    Node* node1Next = node1->pNext;
-    Node* node2Prev = get(list, idxOf(list, node2)-1);
-    Node* node2Next = node2->pNext;
-    printf("\nRUNNING SWAP\n");
-    if(node1Prev == NULL) {
-        list->pHeadNode = node2;
-        node2->pNext = node1Next;
-        node2Prev->pNext = node1;
-        node1->pNext = node2Next;
-    } else if(node2Prev == NULL) {
-        list->pHeadNode = node1;
-        node1->pNext = node2Next;
-        node1Prev->pNext = node2;
-        node2->pNext = node1Next;
-    } else {
-        node1Prev->pNext = node2;
-        node2->pNext = node1Next;
-        node2Prev->pNext = node1;
-        node1->pNext = node2Next;
-    }
-    
+    LetterNode* node1Data = node1->pData;
+    LetterNode* node2Data = node2->pData;
+    node1->pData = node2Data;
+    node2->pData = node1Data;
 }
 
 void replace(LinkedList* list, int pos, Node* newNode) {
@@ -111,6 +94,8 @@ void bubbleSort(LinkedList* list) {
                 swap(list, curr, nextCurr);
                 wasSorted = true;
             }
+            curr = nextCurr;
+            nextCurr = nextCurr->pNext;
         }
     }
 }
